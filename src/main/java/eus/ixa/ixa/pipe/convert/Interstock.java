53e55a07b4412e2ddf0583b7e8487499d0d244f7
Rodrigo Agerri <ragerri@apache.org>
2018-01-23 17:33:56 +0100

    working now

--- src/main/java/eus/ixa/ixa/pipe/convert/Interstock.java
+++ src/main/java/eus/ixa/ixa/pipe/convert/Interstock.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -19,7 +21,25 @@ public class Interstock {
   private Interstock() {
   }
 
-  public static void getJSONBodyElem(String inputFile) throws IOException {
+  public static void getJSONBodyElem(String fileName) throws IOException {
+    JSONParser parser = new JSONParser();
+    Path filePath = Paths.get(fileName);
+    BufferedReader breader = new BufferedReader(
+        Files.newBufferedReader(filePath, StandardCharsets.UTF_8));
+    String line;
+    while ((line = breader.readLine()) != null) {
+      try {
+        Object obj = parser.parse(line);
+        JSONObject jsonObject = (JSONObject) obj;
+        String text = (String) jsonObject.get("body");
+        System.out.println(text);
+      } catch (ParseException e) {
+        e.printStackTrace();
+      }
+    }
+    breader.close();
+  }
+  public static void getJSONElem(String inputFile) throws IOException {
     JSONParser parser = new JSONParser();
     Path filePath = Paths.get(inputFile);
     InputStream inputStream = CmdLineUtil.openInFile(filePath.toFile());
