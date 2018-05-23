7eb2e7c6ae2224fd4e6ee6912434db4f15a702d6
Rodrigo Agerri <ragerri@apache.org>
2018-05-23 18:53:20 +0200

    timeml to conll02 completed

--- src/main/java/eus/ixa/ixa/pipe/convert/TimeMLFormat.java
+++ src/main/java/eus/ixa/ixa/pipe/convert/TimeMLFormat.java
@@ -2,7 +2,6 @@ package eus.ixa.ixa.pipe.convert;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -19,22 +18,21 @@ import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
-import org.jdom2.xpath.XPathFactory;
 
 import eus.ixa.ixa.pipe.ml.tok.Token;
 import ixa.kaflib.KAFDocument;
 
 public class TimeMLFormat {
 
-  private static final Pattern eventPattern = Pattern
-      .compile("(<EVENT.*?>)(.*?)(</EVENT>)");
-  private static final Pattern timex3Pattern = Pattern
-      .compile("<\tTIMEX3.*?type\t=\t\"\t(\\S+)\t\".*?>\t(.*?)\t<\t/TIMEX3\t>");
-  private static final Pattern timexPattern = Pattern
-      .compile("<TIMEX3.*?>(.*?)</TIMEX3>");
+  private static final Pattern eventPattern = Pattern.compile("(<EVENT.*?>)(.*?)(</EVENT>)", Pattern.DOTALL);
+  private static final Pattern timexPattern = Pattern.compile("(<TIMEX3.*?>)(.*?)(</TIMEX3>)");
   private static final Pattern timexTokenizedPattern = Pattern
-      .compile("<\\s+TIMEX3.*?>\\s+(.*?)\\s+<\\s+/TIMEX3\\s+>");
-
+      .compile("<\\s+TIMEX3.*?>\\s+(.*?)<\\s+/TIMEX3\\s+>", Pattern.DOTALL);
+  private static final Pattern timex3Pattern = Pattern
+      .compile("<\tTIMEX3.*?type\t=\t\"\t(\\S+)\t\".*?>\t(.*?)\t<\t/TIMEX3\t>", Pattern.DOTALL);
+  
+  
+                                                                                                                                                                                                                                                                                                                                            
   private TimeMLFormat() {
   }
 
@@ -54,38 +52,37 @@ public class TimeMLFormat {
       xmlOutput.output(textElements, sw);
       StringBuffer sbuffer = sw.getBuffer();
       String text = sbuffer.toString().trim();
-      text = eventPattern.matcher(text).replaceAll("$2");
       // we do not use event elements
+      text = eventPattern.matcher(text).replaceAll("$2");
+      //remove spaces from temporal expression prior tokenization
+      text = convertTimex(text);
       List<List<Token>> tokens = StringUtils.tokenizeDocument(text, language);
       // iterate over the tokenized sentences
       for (List<Token> sentence : tokens) {
         String[] tokensArray = eus.ixa.ixa.pipe.ml.utils.StringUtils
             .convertListTokenToArrayStrings(sentence);
         String sentenceString = StringUtils.getStringFromTokens(tokensArray);
+        //modify timex expression for formatting
         sentenceString = convertSpaceToTabTimex(sentenceString);
-        //System.out.println(sentenceString);
         String[] textArray = sentenceString.split(" ");
         for (int i = 0; i < textArray.length; i++) {
           if (textArray[i].startsWith("<\tTIMEX3")) {
-            //System.out.println(textArray[i]);
             String timexType = timex3Pattern.matcher(textArray[i])
                 .replaceAll("$1").trim();
             String timexText = timex3Pattern.matcher(textArray[i])
                 .replaceAll("$2").trim();
-            //System.out.println(timexText + "\t" + timexType);
-            String[] timexExpression = timexText.split("\t");
+            String[] timexExpression = timexText.split("maitena");
             sb.append(timexExpression[0]).append("\t").append("B-").append(timexType).append("\n");
             for (int j = 1; j < timexExpression.length; j++) {
-              //System.out.println(timexExpression[j] + "\t" + "I-" + timexType);
               sb.append(timexExpression[j]).append("\t").append("I-").append(timexType).append("\n");
             }
           } else {
             sb.append(textArray[i]).append("\t").append("O").append("\n");
           }
-        } sb.append("\n");
+        }
+        sb.append("\n");
       }
     } catch (JDOMException | IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
     Path outfile = Files.createFile(Paths.get(fileName.toString() + ".conll02"));
@@ -94,11 +91,36 @@ public class TimeMLFormat {
     System.err.println(">> Wrote conll02 document to " + outfile);
   }
   
+  private static String convertTimex(String line) {
+    //System.out.println("-> " + line);
+    final Matcher timexMatcher = timexPattern.matcher(line);
+    final StringBuffer sb = new StringBuffer();
+    while (timexMatcher.find()) {
+      timexMatcher.appendReplacement(sb,
+          timexMatcher.group().replaceAll("\\s+", "maitena"));
+    }
+    timexMatcher.appendTail(sb);
+    line = sb.toString();
+    return line;
+  }
+  
+  private static String convertSpaceToTabTimex(String line) {
+    //System.out.println("-> " + line);
+    final Matcher timexMatcher = timexTokenizedPattern.matcher(line);
+    final StringBuffer sb = new StringBuffer();
+    while (timexMatcher.find()) {
+      timexMatcher.appendReplacement(sb,
+          timexMatcher.group().replaceAll("\\s+", "\t"));
+    }
+    timexMatcher.appendTail(sb);
+    line = sb.toString();
+    return line;
+  }
+  
   public static void timeMLToCoNLL2002(Path dir, String language) throws IOException {
-    // process one file
     if (Files.isRegularFile(dir) && !dir.toString().endsWith("conll02")) {
       timeMLToBIO(dir, language);
-    } // process one file
+    }
     else {
       // recursively process directories
       try (DirectoryStream<Path> filesDir = Files.newDirectoryStream(dir)) {
@@ -134,18 +156,6 @@ public class TimeMLFormat {
       e.printStackTrace();
     }
     return kaf.toString();
-  }
-
-  private static String convertSpaceToTabTimex(String line) {
-    final Matcher timexMatcher = timexTokenizedPattern.matcher(line);
-    final StringBuffer sb = new StringBuffer();
-    while (timexMatcher.find()) {
-      timexMatcher.appendReplacement(sb,
-          timexMatcher.group().replaceAll("\\s+", "\t"));
-    }
-    timexMatcher.appendTail(sb);
-    line = sb.toString();
-    return line;
   }
   
 }
